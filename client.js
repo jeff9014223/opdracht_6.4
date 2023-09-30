a84e27f9e3a3903f18e26f92b9e759b294acfee9
jeff9014223 <9014223@student.zadkine.nl>
2023-09-30 15:07:55 +0200

    *

--- client.js
+++ client.js
@@ -6,6 +6,10 @@ function multiplyByFive(a) {
     return a * 5
 }
 
+function calculateMinutes(sec) {
+    return sec / 60
+}
+
 console.log(`Antwoord is: ${calculate(5, 5)}`)
 console.log(`Antwoord is: ${multiplyByFive(5)}`)
-
+console.log(`Antwoord is: ${calculateMinutes(3600)}`)
